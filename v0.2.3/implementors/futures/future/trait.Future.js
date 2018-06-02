d3127fd18614a14ef8421898c1f59344c6beef1b
Julius de Bruijn <julius.debruijn@360dialog.com>
2018-06-02 11:07:21 +0000

    rebuild pages at b73e09b8fd916e3901dd8fbac784f33373100622

---
+++ v0.2.3/implementors/futures/future/trait.Future.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["a2"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/futures/0.1/futures/future/trait.Future.html\" title=\"trait futures::future::Future\">Future</a> for <a class=\"struct\" href=\"a2/client/struct.FutureResponse.html\" title=\"struct a2::client::FutureResponse\">FutureResponse</a>",synthetic:false,types:["a2::client::FutureResponse"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
