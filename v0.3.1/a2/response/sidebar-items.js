15990c94664c10193e87c4f0d30bd083ab4a3a0b
Julius de Bruijn <julius.debruijn@360dialog.com>
2018-06-04 13:35:47 +0000

    rebuild pages at d422ebe23335510481a0e48ef169a0062f1265ef

---
+++ v0.3.1/a2/response/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ErrorReason","A description what went wrong with the push notification."]],"struct":[["ErrorBody","The response body from APNs. Only available for errors."],["Response","The response data from APNs."]]});
