65a2cb7e82a35557790b0c6956425b299ee606a1
Julius de Bruijn <julius.debruijn@360dialog.com>
2018-12-18 19:26:50 +0000

    rebuild pages at 51ba18a3900e85bd66ad0e3d78cb4e9ccc5209a3

--- master/implementors/std/error/trait.Error.js
+++ master/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["a2"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">StdError</a> for <a class=\"enum\" href=\"a2/error/enum.Error.html\" title=\"enum a2::error::Error\">Error</a>",synthetic:false,types:["a2::error::Error"]},];
+implementors["a2"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"a2/error/enum.Error.html\" title=\"enum a2::error::Error\">Error</a>",synthetic:false,types:["a2::error::Error"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
