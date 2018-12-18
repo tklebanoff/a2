364c3ab99b179afafc77b36089da7afb7487a8d7
Julius de Bruijn <julius.debruijn@360dialog.com>
2018-12-18 19:32:38 +0000

    rebuild pages at a948070142121cc9a849f969177d12c141c73d24

---
+++ v0.3.6/a2/client/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Endpoint","The APNs service endpoint to connect."]],"struct":[["Client","Handles requests to and responses from Apple Push Notification service. Connects using a given connector. Handles the needed authentication and maps responses."],["FutureResponse",""]]});
