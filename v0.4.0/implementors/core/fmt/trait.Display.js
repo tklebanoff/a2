fc64faf50d68ebe8622fa89f656d083b48f48770
Julius de Bruijn <julius.debruijn@360dialog.com>
2019-03-15 07:55:14 +0000

    rebuild pages at d856243758553fb09cbd396d61a51d028a9494c6

---
+++ v0.4.0/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["a2"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"a2/enum.Priority.html\" title=\"enum a2::Priority\">Priority</a>",synthetic:false,types:["a2::request::notification::options::Priority"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"a2/error/enum.Error.html\" title=\"enum a2::error::Error\">Error</a>",synthetic:false,types:["a2::error::Error"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"a2/response/enum.ErrorReason.html\" title=\"enum a2::response::ErrorReason\">ErrorReason</a>",synthetic:false,types:["a2::response::ErrorReason"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"a2/client/enum.Endpoint.html\" title=\"enum a2::client::Endpoint\">Endpoint</a>",synthetic:false,types:["a2::client::Endpoint"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ v0.4.0/implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["a2"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.89/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"a2/request/notification/struct.LocalizedAlert.html\" title=\"struct a2::request::notification::LocalizedAlert\">LocalizedAlert</a>&lt;'a&gt;",synthetic:false,types:["a2::request::notification::localized::LocalizedAlert"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.89/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"a2/request/payload/struct.APS.html\" title=\"struct a2::request::payload::APS\">APS</a>&lt;'a&gt;",synthetic:false,types:["a2::request::payload::APS"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.89/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"a2/request/payload/enum.APSAlert.html\" title=\"enum a2::request::payload::APSAlert\">APSAlert</a>&lt;'a&gt;",synthetic:false,types:["a2::request::payload::APSAlert"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
